a421ec9ea79398965b94eb1f4f771799d9e62ff3
devvime <dihapp@outlook.com>
2025-10-07 19:54:23 -0300

    fix: adjust knex connection

--- knexfile.js
+++ knexfile.js
@@ -4,6 +4,8 @@ export default {
   development: {
     client: process.env.DEV_DATABASE_CLIENT,
     connection: {
+      host: process.env.DEV_DATABASE_HOST,
+      port: process.env.DEV_DATABASE_POST,
       user: process.env.DEV_DATABASE_USER,
       password: process.env.DEV_DATABASE_PASSWORD,
       database: process.env.DEV_DATABASE_NAME,
@@ -23,6 +25,8 @@ export default {
   staging: {
     client: process.env.STG_DATABASE_CLIENT,
     connection: {
+      host: process.env.STG_DATABASE_HOST,
+      port: process.env.STG_DATABASE_POST,
       user: process.env.STG_DATABASE_USER,
       password: process.env.STG_DATABASE_PASSWORD,
       database: process.env.STG_DATABASE_NAME,
@@ -42,6 +46,8 @@ export default {
   production: {
     client: process.env.PROD_DATABASE_CLIENT,
     connection: {
+      host: process.env.PROD_DATABASE_HOST,
+      port: process.env.PROD_DATABASE_POST,
       user: process.env.PROD_DATABASE_USER,
       password: process.env.PROD_DATABASE_PASSWORD,
       database: process.env.PROD_DATABASE_NAME,
--- src/app/shared/database.js
+++ src/app/shared/database.js
@@ -1,14 +1,33 @@
 import knex from "knex";
 
+const connection = {
+  host: process.env.DEV_DATABASE_HOST,
+  port: process.env.DEV_DATABASE_POST,
+  user: process.env.DEV_DATABASE_USER,
+  password: process.env.DEV_DATABASE_PASSWORD,
+  database: process.env.DEV_DATABASE_NAME,
+};
+
+switch (process.env.ENVIRONMENT) {
+  case 'stg':
+    connection.host = process.env.STG_DATABASE_HOST;
+    connection.port = process.env.STG_DATABASE_PORT;
+    connection.user = process.env.STG_DATABASE_USER;
+    connection.password = process.env.STG_DATABASE_PASSWORD;
+    connection.database = process.env.STG_DATABASE_NAME;
+    break;
+  case 'prod':
+    connection.host = process.env.PROD_DATABASE_HOST;
+    connection.port = process.env.PROD_DATABASE_PORT;
+    connection.user = process.env.PROD_DATABASE_USER;
+    connection.password = process.env.PROD_DATABASE_PASSWORD;
+    connection.database = process.env.PROD_DATABASE_NAME;
+    break;
+}
+
 const database = knex({
   client: process.env.DEV_DATABASE_CLIENT,
-  connection: {
-    host: process.env.DEV_DATABASE_HOST,
-    port: process.env.DEV_DATABASE_POST,
-    user: process.env.DEV_DATABASE_USER,
-    password: process.env.DEV_DATABASE_PASSWORD,
-    database: process.env.DEV_DATABASE_NAME,
-  },
+  connection,
 });
 
 export default database;
